1900b5c1f591049b7052f1d51c439fcfe5958f95
Artem <Serebryakov>
2022-08-15 16:13:32 +0300

    pasreJSON - done

--- Weather/Model/NetworkWeatherManager.swift
+++ Weather/Model/NetworkWeatherManager.swift
@@ -27,4 +27,18 @@ struct NetworkWeatherManager {
         tast.resume()
         
     }
+    
+    func pasreJSON(withData data: Data) -> Weather? {
+        let decoder = JSONDecoder()
+        do {
+            let weatherData = try decoder.decode(WeatherData.self, from: data)
+            guard let weather = Weather(weatherData: weatherData) else {
+                return nil
+            }
+            return weather
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
 }
--- Weather/Model/Weather.swift
+++ Weather/Model/Weather.swift
@@ -15,8 +15,8 @@ struct Weather {
     var condition: String
     var pressureMm: Int
     var windSpeed: Int
-    var tempMin: Int
-    var tempMax: Int
+    var tempMin: Int = 0
+    var tempMax: Int = 0
     
     var conditionString: String {
         switch condition {
@@ -41,5 +41,16 @@ struct Weather {
         case "thunderstorm-with-hail" : return "гроза с градом"
         default: return "Загрузка..."
         }
+    }
+    
+    init?(weatherData: WeatherData) {
+        temperature = weatherData.fact.temp
+        conditionCode = weatherData.fact.icon
+        url = weatherData.info.url
+        condition = weatherData.fact.condition
+        pressureMm = weatherData.fact.pressureMm
+        windSpeed = weatherData.fact.windSpeed
+        tempMin = weatherData.forecast.first!.parts.day.tempMin!
+        tempMax = weatherData.forecast.first!.parts.day.tempMin!
     }
 }
